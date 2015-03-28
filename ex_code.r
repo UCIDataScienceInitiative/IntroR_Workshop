da6f3056622fb4b7760cbf8550418ca1b38faf52
soapless <zhey@uci.edu>
2015-03-28 14:10:37 -0700

    added key words and more explanations; fixed some issues in the original data that makes read.fwf work incorrectly

--- ex_code.r
+++ ex_code.r
@@ -1,30 +1,27 @@
-# EX0. Set working directory
-# + use setwd() to set the working directory to the folder where the data files are. 
-setwd('data/') # replace "./data/" to the path of the folder where your data files are
-
 # EX1. Import data
-# + 1.1  use read.csv() to import the data "auto-mpg.csv" (the data is of comma separated value file format), and store it as an object "data" ( data = read.csv(....) ). We need to set header = FALSE since the data starts right from the first line (i.e. the data has no header). Look at the data set to see what string/symbols are used to denote missing values; in this data "NA" is used, so na.strings = "NA" should be specified in read.csv(). 
+# + 1.1  find the folder where the data files are saved, and set working directory to that folder using setwd().
+# + 1.2 use read.csv() to import the data "auto-mpg.csv" (the data is of comma separated value file format), and store it as an object "data" ( data = read.csv(....) ). We need to set header = FALSE since the data starts right from the first line (i.e. the data has no header). Look at the data set to see what string/symbols are used to denote missing values; in this data "NA" is used, so na.strings = "NA" should be specified in read.csv().  
 data = read.csv(file = "auto-mpg.csv", header = FALSE, na.strings= "NA")
 
-# + 1.2 (optional) Read data file "auto-mpg.data-original". What happens and why? Check the original data file.
-data_baddata = read.table(file = "auto-mpg.data-original", header = FALSE, sep=" ")
+# + 1.3 (optional) Read data file "auto-mpg.data-original.txt". What happens and why? Check the original data file.
+data2 = read.table(file = "auto-mpg.data-original.txt", header = FALSE, sep=" ")
 # It returns error because the data format is not suitable for read.table or read.csv. It has fixed width format so you need to use read.fwf() and specify the column widths. Therefore you need to know the column widths in advance; if you don't know, you can look at the data and count the widths). Or, you can open the data in Excel which automatically obtains the column widths, convert it to a csv file, and import the data using read.csv(). 
+data2 = read.fwf("data/auto-mpg.data-original.txt", widths = c(7, 5, 11, 11, 11, 7, 5, 3, 50))
 
 # EX2. General check for the data
-# + 2.1 check the top few rows of data; open the original data in excel or notepad and compare with the rows printed in R. 
+# + 2.1 check the top few rows of data; open the original data in excel or notepad and compare with the rows printed in R.  
 head(data)
 # + 2.2. check the dimension of data
 dim(data)
 
 # EX3. Add variable names to data
-# + 3.1 read variable names from "auto-mpg-names.txt" using readLines() and store as an object "varnames" (varnames = readLines(...)). Type "varnames" in the console to see what's inside. The difference between readLines() and read.table() is that readLines() imports the data file into a vector of strings, but read.table() imports the data file into a data frame. Each element of the vector returned by readLines() is the content of a entire line in the data file. 
+# + 3.1 import variable names from "auto-mpg-names.txt" using readLines() and store as an object "varnames" (varnames = readLines(...)). Type "varnames" in the console to see what's inside. The difference between readLines() and read.table() is that readLines() imports the data file into a vector of strings, but read.table() imports the data file into a data frame. Each element of the vector returned by readLines() is the content of an entire line in the data file. 
 varnames = readLines("auto-mpg-names.txt")
 varnames
 # if you used read.table("auto-mpg-names.txt") instead, then you need to extract the actual vector of names out of the dataframe by varnames = varnames[,1] or varnames = varnames[[1]]
-# + 3.2 Run "names(data)". It returns the variable names of the data. Assign the new variable names to data by "names(data) = varnames". 
+# + 3.2 Run "names(data)". It returns the variable names of the data. Assign the new variable names to data by "names(data) = varnames". Check again the variable names, to see if they are updated correctly.
 names(data)
 names(data) = varnames
-# + 3.3 check again the variable names, to see if they are updated correctly. 
 names(data)
 
 # EX4. Data summary
@@ -35,7 +32,7 @@ summary(data)
 
 
 # EX5. Subset exercises 
-# + 5.1 summarize the variable mpg (use summary()). (There are 3 ways you can get the variable mpg.) Do you see something weird in the result? What might be the reason? We will get back to this later. 
+# + 5.1 summarize the variable mpg (use summary()). (There are 3 ways you can get the variable mpg.) Do you see something weird in the result? What might be the reason? We will get back to this later.  
 summary(data$mpg)
 summary(data[, "mpg"])
 summary(data[, 1])
@@ -61,13 +58,13 @@ data  = data[,-9]
 
 
 # EX6. Discrete variables 
-# + 6.1 we are going to change the data, so please first make a copy of the original data (assign data to a new object "data_ori"); in case you do something wrong later you can easily reset the new data to be the original data. 
+# + 6.1 we are going to change the data, so please first make a copy of the original data (assign data to a new object "data_ori"); in case you do something wrong later you can easily reset the new data to be the original data.
 data_ori = data
-# + 6.2 origin is a categorical variable by nature, so we need to convert it to a factor type (use factor()), and assign it back to data$origin. 
+# + 6.2 origin is a categorical variable by nature, so we need to convert it to a factor type (use factor()), and assign it back to data$origin.  
 data$origin = factor(data$origin)
-# + 6.3 Add the labels for the values (1: American, 2: European, 3:Japanese).
+# + 6.3 run "levels(data$origin)" to see the current labels for the variable origin. Change the labels for the values (1: American, 2: European, 3:Japanese). 
 levels(data$origin) = c("American", "European", "Japanese")
-# + 6.4 use summary() on the converted variable and see how the summary output changes for a factor variable. 
+# + 6.4 use summary() on the converted variable and see how the summary output changes for a factor variable.
 
 
 # EX7. Missing values
@@ -107,7 +104,7 @@ n_European = sum(data_noNA$origin == "European")
 # + 9.3 write a for loop that does the following in each iteration:
 #   + find the subset of European cars and Japanese cars
 # + randomize the origin of the cars: randomly sample n_European instances to be "European" and the rest to be "Japanese"
-# + calcualte the new difference in the mean mpg of the randomized data (Jap - Euro)
+# + calculate the new difference in the mean mpg of the randomized data (Jap - Euro)
 # + record whether the new difference >= the observed difference, dif_JapVSEuro
 # + 9.4 Run the simulation of 1000 iterations
 B = 1000
@@ -147,16 +144,16 @@ ggplot(data_noNA_cont_melt, aes(value)) + geom_histogram() +
 dev.off()
 
 
-# EX11. Boxplot of mpg by origin to visually check if mpg is different across different countries of origin. First look up how to make a boxplot in the online ggplot2 documentation. In order for boxplot to work, make sure the variable for the x-axis (origin in this case) is converted to factor type. After you've generated the plots, you can see mpg does look different across different origin categories, suggesting mpg is likely to depend on the car origin. We will do a formal statistical test later.   
+# EX11. Boxplot to check relationship between a continuous variable and a categorical variable
+# Boxplot of mpg by origin to visually check if mpg is different across different countries of origin. First look up how to make a boxplot in the online ggplot2 documentation. In order for boxplot to work, make sure the variable for the x-axis (origin in this case) is converted to factor type. After you've generated the plots, you can see mpg does look different across different origin categories, suggesting mpg is likely to depend on the car origin. We will do a formal statistical test later.
 ggplot(data_noNA, aes(origin, mpg)) + geom_boxplot()
 
 # + (optional) add an additional layer a)geom_point or b)geom_jitter() and see what happens
 ggplot(data_noNA, aes(origin, mpg)) + geom_boxplot() + geom_jitter()
 ggplot(data_noNA, aes(origin, mpg)) + geom_boxplot() + geom_point()
 
-
-#   EX12. Scatterplot of mpg vs cylinders to check what the relationship is like, and to check if it is suitable to treat cylinder as a numerical variable or categorical variable.
-# + 12.1. check the use of stat_smooth() and the argument "method". Generate a scatter plot with the default smooth curve fit overlayed, and the other scatter plot with a linear regression fit overlayed (method="lm"). The two fitted curves should both have non-zero slopes but look quite different, suggesting mpg and cylinders are associated, but not linearly associated, in which case we want to keep cylinder as a categorical variable. You can see very few cases have cylinder = 3 or 5; sometimes you may want to do a secondary analysis with those cases removed. 
+#   EX12. Scatterplot to check what the relationship of the two variables is like, e.g. linear or not.
+# + 12.1. Scatterplot of mpg vs cylinders to check the relationship and see if it is suitable to treat cylinder as a numerical variable (linear) or categorical variable (not linear). For this task, first check the use of stat_smooth() and the argument "method". Generate a scatter plot with the default smooth curve fit overlayed, and the other scatter plot with a linear regression fit overlayed (method="lm"). The two fitted curves should both have non-zero slopes but look quite different, suggesting mpg and cylinders are associated, but not linearly associated, in which case we want to keep cylinder as a categorical variable. You can see very few cases have cylinder = 3 or 5; sometimes you may want to do a secondary analysis with those cases removed. 
 ggplot(data_noNA, aes(cylinders, mpg)) + geom_point() + stat_smooth()
 ggplot(data_noNA, aes(cylinders, mpg)) + geom_point() + stat_smooth(method="lm")
 # + 12.2 (optional) create another data with instances with odd number of cylinders removed, and check the above plots again. The two fitted curves look similar -> could treat cylinder as numerical. 
@@ -167,7 +164,7 @@ ggplot(data_noNA_evenCyl, aes(cylinders, mpg)) + geom_point() + stat_smooth(meth
 
 
 # EX13. Scatterplot matrix 
-# + Apply function pairs() or ggpairs() on the data to create the scatterplot matrix. If you use ggpairs(), you need to install and load R package "GGally".
+# + Apply function pairs() or ggpairs() on the data to create the scatterplot matrix. Save the plot in a pdf file. If you use ggpairs(), you need to install and load R package "GGally".
 # + to check the relationship between any pair of variable
 # + to check for linearity assumption and homogeneity assumption
 # + If violated, data transformation will be needed when building a linear regression model
@@ -181,15 +178,15 @@ ggpairs(data_noNA)
 
 # EX14. Data transformation
 # + Based on scatterplot matrix, we see increasing variance as mpg increases, and also non-linear relationship between mpg and other variables. We need to transform the variables.
-# + Add new variables to the data: 
-# + (a) log transformed versions of mpg, horsepower, displacement, and weight. Hint: to add a new variable, e.g. log of mpg, you can run data_noNA$logmpg = log(data_noNA$mpg), where "logmpg" is the name for the new variable you choose. 
+# + Add the following new variables to the data. 
+# + (a) log transformed versions of mpg, horsepower, displacement, and weight; name the new variables as logmpg, loghorsepower, etc. Hint: to add a new variable, e.g. log of mpg, you can assign 'log(data_noNA$mpg)' to 'data_noNA$logmpg'. 
 data_noNA$logmpg = log(data_noNA$mpg)
 data_noNA$loghorsepower = log(data_noNA$horsepower)
 data_noNA$logdisplacement = log(data_noNA$displacement)
 data_noNA$logweight = log(data_noNA$weight)
 # + (b) a factor version of cylinders.
 data_noNA$cylinders_cat = factor(data_noNA$cylinders)
-
+  
 
 #   EX15. Statistical analysis  
 # + 15.1 ANOVA for origin. To formally test whether mean mpg is different across cars of the three origins, use significance level 0.05. First build a linear regression for mpg against origin. And then use both ANOVA() and summary() to check the results. 
@@ -211,7 +208,12 @@ summary(model_log)
 # + 15.4 Which column is to answer the relationship between mpg and other variables? 
 # the column "Estimate". The number tells you on average how many units of increase in mpg is associated with one unit increase in the corresponding variable.  
 
-# + 15.5 Diagnostics (important in statistical analysis). Execute plot(model) and plot(model_log) in R and check the four plots for each model. You can find the specific help file for "plot()" for a lm object by executing "?plot.lm". Based on the diagnostic plots, are these reasonable models? Which one is better? Check the following aspects.  
+# + 15.5 Run "newcase = data_noNA[1:10,]" to take the first 10 instances, and treat them as some new cars for which we want to predict mpg. Use predict() to predict mpg for them using respectively the object "model" and "model**_**log". Keep in mind that from "model_log", predict() returns the predictions for log(mpg) instead of mpg. 
+newcase = data_noNA[1:10,]
+predict(model, newcase)
+exp(predict(model_log, newcase))
+
+# + 15.6 Diagnostics (important in statistical analysis). Execute plot(model) and plot(model_log) in R and check the four plots for each model. You can find the specific help file for "plot()" for a lm object by executing "?plot.lm". Based on the diagnostic plots, are these reasonable models? Which one is better? Check the following aspects.  
 # + (a) linearity assumption, (b) normality assumption, (c) constant variance (d) outliers
 plot(model)
 plot(model_log)
